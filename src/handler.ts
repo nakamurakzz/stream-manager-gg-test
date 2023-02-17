5fa62fc0da2ab2dcf88a15c8f5fa1e40ed3b40b0
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-02-17 22:27:21 +0900

    pythonのサンプルコンポーネント作成

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,9 @@
-console.log("custom component start")
+console.log("custom component start");
 
 export const index = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  console.log(JSON.stringify(event, null, 2))
-  console.log("hello world")
+  console.log(JSON.stringify(event, null, 2));
+  console.log("hello world");
   callback(undefined, {});
   return;
 };
